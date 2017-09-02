e5d02447a2b1760d6ac7e29ce78583333775b3f3
Yinan Zhu <zynsteve@tamu.edu>
2017-09-01 23:15:18 -0500

    search for a range

---
+++ src/basicAlgorithmAndDataStructure2/SearchForARange.java
@@ -0,0 +1,71 @@
+package basicAlgorithmAndDataStructure2;
+
+/**
+ * Given a sorted array of n integers, find the starting and ending position of a given target value.
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * Example
+ * Given [5, 7, 7, 8, 8, 10] and target value 8,
+ * return [3, 4].
+ */
+
+public class SearchForARange {
+    /** 
+     *@param A : an integer sorted array
+     *@param target :  an integer to be inserted
+     *return : a list of length 2, [index1, index2]
+     */
+    public int[] searchRange(int[] A, int target) {
+        if (A.length == 0) {
+            return new int[]{-1, -1};
+        }
+        
+        int start, end, mid;
+        int[] bound = new int[2];
+        
+        // search for left bound
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[start] == target) {
+            bound[0] = start;
+        } else if(A[end] == target) {
+            bound[0] = end;
+        } else {
+            bound[0] = bound[1] = -1;
+        }
+        
+        // search for right bound
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                start = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[end] == target) {
+            bound[1] = end;
+        } else if (A[start] == target) {
+            bound[1] = start;
+        } else {
+            bound[0] = bound[1] = -1;
+            return bound;
+        }
+        
+        return bound;
+    }
+}
